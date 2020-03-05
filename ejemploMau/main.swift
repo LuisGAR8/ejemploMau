89066f38e80d5b17a6f205d2e7fbeea0c54bac54
Luis González <luisalexis98@hotmail.com>
2020-03-04 18:35:42 -0600

    Mensaje commit 1

---
+++ ejemploMau/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  ejemploMau
+//
+//  Created by lgonzalez on 3/4/20.
+//  Copyright © 2020 lgonzalez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+print("XD")
